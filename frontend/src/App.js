a6afc97ba1520fe5d4b66914358e65361513e77c
NgocLuan <2051052077luan@ou.edu.vn>
2023-09-07 22:03:24 +0700

    first commit

---
+++ frontend/src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './App.css';
+
+import "bootstrap/dist/css/bootstrap.min.css";
+import Layout from './Compoments/Layout';
+
+
+
+
+const App = () => {
+  return (
+    <>
+      <Layout />
+    </>
+  )
+}
+
+export default App;
